38a35791caa2972a4a20d1edf24bf2e9dd219e5c
Casper Weiss Bang <casper.bang@centrica.com>
2022-04-16 14:16:06 +0200

    Recreated ComparisonCompactor from chapter 15

---
+++ CleanCode.Tests/ComparisonCompactorTests.cs
@@ -0,0 +1,133 @@
+using CleanCode;
+using NUnit.Framework;
+
+namespace CleanCode.Tests;
+
+public class ComparisonCompactorTests
+{
+    [Test]
+    public void TestMessage()
+    {
+        var failure = new ComparisonCompactor(0, "b", "c").FormatCompactedComparison("a");
+        Assert.That(failure, Is.EqualTo("a expected:<[b]> but was:<[c]>"));
+    }
+    
+    [Test]
+    public void TestStartSame()
+    {
+        var failure = new ComparisonCompactor(1, "ba", "bc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<b[a]> but was:<b[c]>"));
+    }
+    
+    [Test]
+    public void TestEndSame()
+    {
+        var failure = new ComparisonCompactor(1, "ab", "cb").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<[a]b> but was:<[c]b>"));
+    }
+    
+    [Test]
+    public void TestSame()
+    {
+        var failure = new ComparisonCompactor(1, "ab", "ab").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<ab> but was:<ab>"));
+    }
+    
+    [Test]
+    public void TestNoContextStartAndEndSame()
+    {
+        var failure = new ComparisonCompactor(0, "abc", "adc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<...[b]...> but was:<...[d]...>"));
+    }
+    
+    [Test]
+    public void TestStartAndEndContext()
+    {
+        var failure = new ComparisonCompactor(1, "abc", "adc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<a[b]c> but was:<a[d]c>"));
+    }
+    
+    [Test]
+    public void TestStartAndEndContextWithEllipses()
+    {
+        var failure = new ComparisonCompactor(1, "abcde", "abfde").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<...b[c]d...> but was:<...b[f]d...>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorStartSameComplete()
+    {
+        var failure = new ComparisonCompactor(2, "ab", "abc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<ab[]> but was:<ab[c]>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorEndSameComplete()
+    {
+        var failure = new ComparisonCompactor(2, "bc", "abc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<[]bc> but was:<[a]bc>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorOverlappingMatches()
+    {
+        var failure = new ComparisonCompactor(0, "abc", "abbc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<...[]...> but was:<...[b]...>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorOverlappingMatchesContext()
+    {
+        var failure = new ComparisonCompactor(2, "abc", "abbc").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<ab[]c> but was:<ab[b]c>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorOverlappingMatches2()
+    {
+        var failure = new ComparisonCompactor(0, "abcdde", "abcde").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<...[d]...> but was:<...[]...>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorOverlappingMatches2Context()
+    {
+        var failure = new ComparisonCompactor(2, "abcdde", "abcde").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<...cd[d]e> but was:<...cd[]e>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorWithActualNull()
+    {
+        var failure = new ComparisonCompactor(0, "a", null).FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<a> but was:<null>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorWithActualNullContext()
+    {
+        var failure = new ComparisonCompactor(2, "a", null).FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<a> but was:<null>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorWithExpectedNull()
+    {
+        var failure = new ComparisonCompactor(0, null, "a").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<null> but was:<a>"));
+    }
+    
+    [Test]
+    public void TestComparisionErrorWithExpectedNullContext()
+    {
+        var failure = new ComparisonCompactor(2, null, "a").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<null> but was:<a>"));
+    }
+    
+    [Test]
+    public void TestBug609972()
+    {
+        var failure = new ComparisonCompactor(10, "S&P500", "0").FormatCompactedComparison(null);
+        Assert.That(failure, Is.EqualTo("expected:<[S&P50]0> but was:<[]0>"));
+    }
+}
---
+++ CleanCode/ComparisonCompactor.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+
+namespace CleanCode;
+
+public class ComparisonCompactor
+{
+    private const string ELLIPSIS = "...";
+    private const char DELTA_END = ']';
+    private const char DELTA_START = '[';
+    private readonly int _contextLength;
+    private readonly string? _expected;
+    private readonly string? _actual;
+    private int _suffixLength;
+    private int _prefixLength;
+
+    public ComparisonCompactor(int contextLength, string? expected, string? actual)
+    {
+        _contextLength = contextLength;
+        _expected = expected;
+        _actual = actual;
+    }
+
+    public string FormatCompactedComparison(string? message)
+    {
+        var compactExpected = _expected;
+        var compactActual = _actual;
+        if (ShouldBeCompacted())
+        {
+            FindCommonPrefixAndSuffix();
+            compactExpected = Compact(_expected!);
+            compactActual = Compact(_actual!);
+        }
+
+        return Format(message, compactExpected, compactActual);
+    }
+
+    private bool ShouldBeCompacted() => !ShouldNotBeCompacted();
+    
+    private bool ShouldNotBeCompacted() => _expected is null || _actual is null || _expected.Equals(_actual);
+
+
+    private void FindCommonPrefixAndSuffix()
+    {
+        if (_expected is null || _actual is null)
+            throw new InvalidOperationException();
+        
+        FindCommonPrefix();
+        _suffixLength = 0;
+        for (; !SuffixOverlapsPrefix(); _suffixLength++)
+        {
+            if (CharFromEnd(_expected, _suffixLength) != CharFromEnd(_actual, _suffixLength))
+                break;
+        }
+    }
+
+    private static char CharFromEnd(string s, int i) => s[s.Length - i - 1];
+
+    private bool SuffixOverlapsPrefix()
+    {
+        if (_expected is null || _actual is null)
+            throw new InvalidOperationException();
+        
+        return _actual.Length - _suffixLength <= _prefixLength || _expected.Length - _suffixLength <= _prefixLength;
+    }
+
+    private void FindCommonPrefix()
+    {
+        if (_expected is null || _actual is null)
+            throw new InvalidOperationException();
+        
+        _prefixLength = 0;
+        var end = Math.Min(_expected.Length, _actual.Length);
+        for (; _prefixLength < end; _prefixLength++)
+        {
+            if (_expected[_prefixLength] != _actual[_prefixLength])
+                break;
+        }
+            
+    }
+
+    private string Compact(string s) =>
+        new StringBuilder()
+            .Append(StartingEllipsis())
+            .Append(StartingContext())
+            .Append(DELTA_START)
+            .Append(Delta(s))
+            .Append(DELTA_END)
+            .Append(EndingContext())
+            .Append(EndingEllipsis())
+            .ToString();
+
+
+    private string StartingEllipsis() => _prefixLength > _contextLength ? ELLIPSIS : "";
+    
+    private string StartingContext()
+    {
+        if (_expected is null)
+            throw new InvalidOperationException();
+        
+        var contextStart = Math.Max(0, _prefixLength - _contextLength);
+        var contextEnd = _prefixLength;
+        return JavaStyleSubstring(_expected, contextStart, contextEnd);
+    }
+    
+    private string Delta(string s)
+    {
+        var deltaStart = _prefixLength;
+        var deltaEnd = s.Length - _suffixLength;
+        return JavaStyleSubstring(s, deltaStart, deltaEnd);
+    }
+
+    private string EndingContext()
+    {
+        if (_expected is null)
+            throw new InvalidOperationException();
+
+        var contextStart = _expected.Length - _suffixLength;
+        var contextEnd = Math.Min(contextStart + _contextLength, _expected.Length);
+        
+        return JavaStyleSubstring(_expected, contextStart, contextEnd);
+    }
+    
+    
+    private string EndingEllipsis() => _suffixLength > _contextLength ? ELLIPSIS : "";
+    
+    private static string Format(string? message, string? expected, string? actual)
+    {
+        var prefix = message is not null ? $"{message} " : "";
+        return $"{prefix}expected:<{FormatNullable(expected)}> but was:<{FormatNullable(actual)}>";
+    }
+
+    private static string FormatNullable(string? s) => s ?? "null";
+        
+
+    private static string JavaStyleSubstring(string s, int beginIndex,
+      int endIndex)
+    {
+      var len = endIndex - beginIndex;
+      return s.Substring(beginIndex, len);
+    }
+    
+}
